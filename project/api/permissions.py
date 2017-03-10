6c661a4db4be2cfa97d4bc9ad2b4fffb9a32b02d
Dylan Stein <djstein@ncsu.edu>
2017-03-10 15:52:34 -0500

    Add permissions for Channels and Message editing

--- project/api/permissions.py
+++ project/api/permissions.py
@@ -12,5 +12,36 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
         if request.method in permissions.SAFE_METHODS:
             return True
 
-        # Write permissions are only allowed to the owner of the snippet
+        # Write permissions are only allowed to the owner of the message
         return obj.owner == request.user
+
+
+class IsUser(permissions.BasePermission):
+    """
+    Custom permission to only allow users to edit their own user object
+    """
+
+    def has_object_permission(self, request, view, obj):
+        # Read permissions are allowed to any request,
+        # so we'll always allow GET, HEAD or OPTIONS requests.
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        # Write permissions are only allowed to the owner of the message
+        return obj == request.user
+
+
+class IsOwner(permissions.BasePermission):
+    """
+    Custom permission to only allow owners of an object to edit it
+    """
+
+    def has_object_permission(self, request, view, obj):
+        # Write permissions are only allowed to the owner of the message
+        return obj.owner == request.user
+
+
+class IsUserIncluded(permissions.BasePermission):
+
+    def has_object_permission(self, request, view, obj):
+        return request.user in set(obj.users.all())
