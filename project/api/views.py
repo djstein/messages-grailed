cf77f9580518b9b396100a8a7c7eaaaebe8581ff
Dylan Stein <djstein@ncsu.edu>
2017-03-10 15:28:56 -0500

    Create custom view/list/retrieve viewset

--- project/api/views.py
+++ project/api/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import User
 from project.api.models import Channel, Message
 from project.api.permissions import IsOwnerOrReadOnly
 from project.api.serializers import  ChannelSerializer, MessageSerializer, UserSerializer
-from rest_framework import permissions, viewsets
+from rest_framework import permissions, viewsets, mixins
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
@@ -14,6 +14,16 @@ def api_root(request, format=None):
         'users': reverse('user-list', request=request, format=format),
         'messages': reverse('messages-list', request=request, format=format),
     })
+
+
+class CreateListRetrieveViewSet(mixins.CreateModelMixin, mixins.ListModelMixin, mixins.RetrieveModelMixin, viewsets.GenericViewSet):
+    """
+    A viewset that provides `retrieve`, `create`, and `list` actions.
+
+    To use it, override the class and set the `.queryset` and
+    `.serializer_class` attributes.
+    """
+    pass
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -29,7 +39,7 @@ class ChannelViewSet(viewsets.ModelViewSet):
     serializer_class = ChannelSerializer
 
 
-class MessageViewSet(viewsets.ModelViewSet):
+class MessageViewSet(CreateListRetrieveViewSet):
     """
     This viewset automatically provides 'list', 'create', 'retrieve', 'update', and 'destory actions'
 
@@ -37,7 +47,7 @@ class MessageViewSet(viewsets.ModelViewSet):
     """
     queryset = Message.objects.all()
     serializer_class = MessageSerializer
-    permission_class = (permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly,)
+    permission_class = (IsOwnerOrReadOnly,)
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
